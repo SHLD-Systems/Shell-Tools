3dd21d5fde3ecb0dfa735d5e278d12f048edd534
Dario Johansson <dario.johansson@outlook.com>
2025-11-19 21:15:39 +0100

    modifications in logic see diff

--- tools/sshfs-automount/service-builder.sh
+++ tools/sshfs-automount/service-builder.sh
@@ -5,21 +5,69 @@ set -e
 start_folder="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 svc_install_dir="/etc/systemd/system"
 
-echo "This utility will build a systemd service file for your SSH Net‑Share script."
-read -p "Enter install folder (enter for current folder): " install_folder
-read -p "Enter service name (without .service): " svc_name
-read -p "Enter service user: " svc_user
-read -p "Enter remote host argument (-h …): " arg_host
-read -p "Enter remote user argument (-u …): " arg_user
-read -p "Enter local “-m” path: " arg_m
-read -p "Enter remote mount “-r” path: " arg_r
-read -p "Enter creds file path (enter for same as installation dir): " arg_p
+auto_arg=false # flag to branch interactive / programmatic settings
 
+# Assignment option - for IaaC
+while [[ $# -gt 0 ]]; do
+	auto_arg=true 
+	case $1 in
+		"-r")
+		shift
+		arg_r="$1"
+		shift
+		;;
+		"-m")
+		shift
+		arg_m="$1"
+		shift
+		;;
+		"-u")
+		shift
+		arg_user="$1"
+		shift
+		;;
+		"-x")
+		shift
+		arg_x="$1"
+		shift
+		;;
+		"-p")
+		shift
+		arg_port="$1"
+		shift
+		;;
+		"-h")
+		shift
+		arg_host="$1"
+		shift
+		;;
+		*)
+		echo "Option Unrecognized: $1"
+		exit 1
+	esac
+done
+
+if [[ $auto_arg == false ]]; then
+	echo "This utility will build a systemd service file for your SSH Net‑Share script."
+	read -p "Enter install folder (default - current folder): " install_folder
+	read -p "Enter service name: " svc_name
+	read -p "Enter service user: " svc_user
+	read -p "Enter remote host argument: " arg_host
+	read -p "Enter remote host port argument (default - 22):" arg_port
+	read -p "Enter remote user argument: " arg_user
+	read -p "Enter local path: " arg_m
+	read -p "Enter remote mount path: " arg_r
+	read -p "Enter creds file path (default - same as installation dir): " arg_x
+fi
 
 # Checking presence of executable
 if [ ! -e "${start_folder}/net-share-SSH.sh" ]; then
 echo "Critical: ${start_folder}/net-share-SSH.sh  not found. File is expected to exist. Aborting."
 exit 1
+fi
+
+if [ -z $arg_port ]; then
+	arg_port=22
 fi
 
 
@@ -34,33 +82,33 @@ else
 fi
 
 # Checking credential file settings. This file can be put in the same folder of binaries, or kept separately.
-# First checks if the variable was set, otherwise it sets arg_p to be in the same as the install folder.
+# First checks if the variable was set, otherwise it sets arg_x to be in the same as the install folder.
 # Second checks if the cred file exists already for some reason. IF not, it let's the user create it via the template.
 # In all these cases, the file is set to strict permissions for reading.
 
-if [ -z $arg_p ]; then
-arg_p="${install_folder}/creds.txt"
+if [ -z $arg_x ]; then
+arg_x="${install_folder}/creds.txt"
 fi
 
-if [ ! -e $arg_p ]; then
+if [ ! -e $arg_x ]; then
 
-	if [ ! -d $(dirname "$arg_p") ]; then
+	if [ ! -d $(dirname "$arg_x") ]; then
 	echo "Path to creds file non-existent, creating file in install folder instead."
-	arg_p="${install_folder}/creds.txt"
+	arg_x="${install_folder}/creds.txt"
 	fi
 
 	echo "Creating Credentials File..."
-	> "${arg_p}"
-	echo "#Add your Network Share Secret Here" >> "${arg_p}"
-	echo "password=" >> "${arg_p}"
-	nano "${arg_p}"
-	chmod u=r,go-rwx "${arg_p}"
+	> "${arg_x}"
+	echo "#Add your Network Share Secret Here" >> "${arg_x}"
+	echo "password=" >> "${arg_x}"
+	nano "${arg_x}"
+	chmod u=r,go-rwx "${arg_x}"
 else
-	chmod u=r,go-rwx "${arg_p}"
+	chmod u=r,go-rwx "${arg_x}"
 fi
 
 # Construct ExecStart command
-exec_cmd=("${install_folder}/net-share-SSH.sh" -u "${arg_user}" -h "${arg_host}" -m "${arg_m}" -r "${arg_r}" -p "${arg_p}")
+exec_cmd=("${install_folder}/net-share-SSH.sh" -u "${arg_user}" -h "${arg_host}" -m "${arg_m}" -r "${arg_r}" -x "${arg_x}" -p "${arg_port}")
 svc_file="${svc_install_dir}/${svc_name}.service"
 
 read -p "Perform ssh connectivity check for validity of credentials and presence of available storage? y/n  " cond_test
